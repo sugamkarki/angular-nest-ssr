0a1a421be694fadd8fc5bfe7dabb088b4e6615f1
Sugam Karki <sugamkarki7058@gmail.com>
2022-05-15 18:22:08 +0100

    build finished

--- server/src/main-ssr.ts
+++ server/src/main-ssr.ts
@@ -4,14 +4,13 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppSSRModule } from './app-ssr.module';
 import { Logger } from '@nestjs/common';
-import { setAppDB } from './setAppDB';
 
 async function bootstrap() {
   const logger = new Logger('boostrap');
 
   const app = await NestFactory.create<NestExpressApplication>(AppSSRModule);
 
-  setAppDB(app);
+  app.setGlobalPrefix('api');
 
   await app.listen(process.env.PORT);
   logger.log('App listening on port ' + process.env.PORT);
